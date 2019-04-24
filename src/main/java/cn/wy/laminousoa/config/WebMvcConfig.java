7a0b6edab517b224460ea9df9f473fd97a94f733
sunyang401769@126.com <superdog333>
2019-04-24 15:32:38 +0800

    设置index.html为默认首页

---
+++ src/main/java/cn/wy/laminousoa/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package cn.wy.laminousoa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * 设置首页为index.html
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+}
